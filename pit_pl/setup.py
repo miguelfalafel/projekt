b8939951a5dfbaa320eeafc01f887e6a6b95769f
miguelfalafel <100212129+miguelfalafel@users.noreply.github.com>
2022-02-24 17:43:15 +0100

    chg10

--- pit_pl/setup.py
+++ pit_pl/setup.py
@@ -1,2 +1,12 @@
 import setuptools
 from setuptools import find_packages
+
+setup(
+    name='pit_p',
+    version='1.00',
+    license='BSD 2-clause',
+    author='Michał Jachimiak',
+    author_email='m.jachmiak3@student.uw.edu.pl',
+    packages=find_packages(),
+    install_requires=["pandas", "numpy", "matplotlib", "openpyxl", "xlrd", "os"]
+)
