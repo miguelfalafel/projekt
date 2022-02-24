9e4c4b54a0f7e85934b990b1aa755490ab314e3c
miguelfalafel <100212129+miguelfalafel@users.noreply.github.com>
2022-02-24 15:11:26 +0100

    chg4

--- pit_pl/pit_package/dane_export/export_dane.py
+++ pit_pl/pit_package/dane_export/export_dane.py
@@ -1,4 +1,4 @@
 #eksport plików do excela
-def export_excel(df, path):
-    df.to_excel(excel_writer=path + "/" + df.name + ".xlsx", engine="openpyxl")
+def export_excel(df, path, nazwa):
+    df.to_excel(excel_writer=path + "\\" + nazwa + ".xlsx", engine="openpyxl")
     return 0
